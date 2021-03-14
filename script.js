5adca3ac88e3c6f60b435993734b3a302447995b
Karen <karen198791@gmail.com>
2021-03-14 19:13:01 +0400

    homework2

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-// 1. Ստեղծել homework1.js ֆայլ որը աշխատեցնելուց հետո __filename գլոբալի  միջոցով console-ում արտածել ֆայլի բացարձակ ճանապարը և  ճանապարհի անվան երկարությունը
-console.log(2+2);
+// 1.  Գրել ծրագիր որը աշխատացնելիս console-ում կարտածի ամբողջ օպերատիվ հիշողությունը (total amount of system memory):
+const os = require('os');
+const fs = require('fs');
+const { getHashes } = require('crypto');
 
-// 2. Ստեղծել package.json  ֆայլ որը կունենա  home2.jpg տեսքը, որտեղ author դիմաց կլինի Ձեր անուն ազգանունը:
-// homework2.js ֆայլի միջոցով օգտագործելով random փաթեթը  console-ում արտածել 5 պատհական int արժեքներ 0 - 100 միջակայքում:
+// console.log(os.totalmem());
 
-// 3. Modules պապակայում ստեղծել currentTime մոդուլ currentTime ֆունկցիայով որը արտածում է  կանչելու պահին ժամը, րոպեն ,վարկյանը և միլիվայրկյանը :
-// Մոդուլը ներմուծել homework3.js ֆայլում և կանչել currentTime ֆունկցիավ  ու արդյունքը արտածել console-ում:
+// 2. Գրել ծրագիր որ աշխատացնելիս console-ում կարտածի IPv4 IP հասցեն(os.networkInterfaces()):
 
-// 4.Modules պապակայում ստեղծել wordsFromString մոդուլ wordsFromString ֆունկցիայով որը տրված նախադասությունից console-ում  արտածում է այն բառերը որոնք չունեն հատուկ սիմվոլ: 
-// Մոդուլը ներմուծել homework4.js ֆայլում և wordsFromString ֆունկցիավ կանչել որևէ նախադասության համար:  
+// console.log(os.networkInterfaces());
+
+// 3.Գրել ծրագիր որը աշխատացնելիս կստեղծի 8 նիշանոց պատահական թվերից կազմված անուն ունեցող txt ֆայլ և այդ ֆայի մեջ Օպերացիան համակարգի central processing unit (CPU) քանակը(os.cpus()):
+let mat = Math.floor(Math.random()*100000000)+1;
+let st = mat + "txt";
+fs.writeFileSync(`${st}`,"");
+os.cpus();
 
 
-// 5.Modules պապակայում ստեղծել charOneSecondOutput մոդուլ charOneSecondOutput ֆունկցիայով որը console-ում  արտածում է ֆունկցիային տրված բառի տառերը 1 վայրկյան պարբերությամբ: 
 
-// Մոդուլը ներմուծել homework5.js ֆայլում և charOneSecondOutput ֆունկցիա կանչել Ձեր անուն ազգանունից  կազմված բառակապակցության համար : 
+// 4.Գրել ծրագիր որ աշխատացնելիս  input.txt կկարդա պարունակությունը , որից հետո այդ պարունակության բոլոր բացատները կփոխարին - ներով: Այնուհետև գծիկներով տեքստ գրել write.txt ֆայլում:
+
+// let radMe = fs.readFileSync('input.txt', 'utf-8');
+// let red = radMe.split(' ');
+// const str = red.join('-');
+// fs.writeFileSync("write.txt", str);
+
+// 5.Գրել ծրագիր որ աշխատացնելիս sync կկարադա նախորդ չորս տնայիների ֆայլերի պարունակությունը(Օրինակ homework1.js, homework2.js, homework3.js, homework4.js): 
+// Այնուհետև ծրագիրը պետք է ստեղծի txt ֆայլ, որի անուն կազմված կլնի օպերացոն համակարգի անունից և այդ պահին ժամ րոպե վարկյանից(win32_14_14_58.txt)   
+// և այդ ֆայլում գրել նախորդ ֆայլերի պարունակությունը:
+// let homework1 = fs.readFileSync('homework1.js', 'utf-8');
+// let homework2 = fs.readFileSync('homework2.js', 'utf-8');
+// let homework3 = fs.readFileSync('homework3.js', 'utf-8');
+// let homework = homework1 + homework2 + homework3;
+// let path = os.platform;
+// let data = new Date();
+// let data1 = data.getHours();
+// let data2 = data.getMinutes();
+// let data3 = data.getSeconds();
+// let str2 = path + "_" + data1 + "_" + data2 + "_" +data3+ "." + "txt";
+// fs.writeFileSync(`${str2}`, homework);
